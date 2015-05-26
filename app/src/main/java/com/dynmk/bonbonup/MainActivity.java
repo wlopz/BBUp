87e4fcb1b63fe27a40731390307057f5267e66f3
wlopz <jrlopez85@gmail.com>
2015-05-26 15:49:50 -0400

    - Modified nav drawer icons - Changed background in details

--- app/src/main/java/com/dynmk/bonbonup/MainActivity.java
+++ app/src/main/java/com/dynmk/bonbonup/MainActivity.java
@@ -60,10 +60,10 @@ public class MainActivity extends ActionBarActivity {
         // Menu
         navDrawerItems.add(new NavDrawerItem(navMenuTitles[0], navMenuIcons.getResourceId(2, -1)));
         // Contacto
-        navDrawerItems.add(new NavDrawerItem(navMenuTitles[1], navMenuIcons.getResourceId(1, -1)));
-        navDrawerItems.add(new NavDrawerItem("Jeans", navMenuIcons.getResourceId(3, -1)));
-        navDrawerItems.add(new NavDrawerItem("Blusas", navMenuIcons.getResourceId(4, -1)));
-        navDrawerItems.add(new NavDrawerItem("Leggings", navMenuIcons.getResourceId(5, -1)));
+        navDrawerItems.add(new NavDrawerItem(navMenuTitles[1], navMenuIcons.getResourceId(3, -1)));
+        navDrawerItems.add(new NavDrawerItem("Jeans", navMenuIcons.getResourceId(4, -1)));
+        navDrawerItems.add(new NavDrawerItem("Blusas", navMenuIcons.getResourceId(5, -1)));
+        navDrawerItems.add(new NavDrawerItem("Leggings", navMenuIcons.getResourceId(6, -1)));
         // Catologo
         //navDrawerItems.add(new NavDrawerItem(navMenuTitles[2], navMenuIcons.getResourceId(2, -1), true, "Estrenos"));
         // old Contacto (Pedidos)
